959e584fb8194294f85578791715121c68c0abc3
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-12-01 18:49:01 +0900

    make it stylish dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css';
 import { ReactNode } from 'react';
+// ★追加: SpeedInsightsをインポート
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* ★追加: ここに配置します */}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
